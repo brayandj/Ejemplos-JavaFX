280b63cd616bf7cef7441546bd0c27b444c2e4d6
bryandj <bryandjw6@gmail.com>
2023-12-29 09:30:18 -0600

    Ejemplos con Layout

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,16 +18,19 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        VBox root = new VBox();
-        root.setSpacing(10);
-        root.setAlignment(Pos.CENTER);
-        Label label = new Label("Haz click sobre el botón");
-        Button button = new Button("Agregar");
-        root.getChildren().addAll(label, button);
-        button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> label.setText("Has dado click sobre el botón"));
-        Scene scene = new Scene(root, 550, 500);
+        Group group = new Group();
+        Button one = new Button("one");
+        Button two = new Button("thow");
+        one.setLayoutX(100);
+        one.setLayoutY(10);
+        two.layoutXProperty().bind(one.layoutXProperty().add(one.widthProperty().add(10)));
+        two.layoutYProperty().bind(one.layoutYProperty());
+        group.setEffect(new DropShadow());
+        group.setRotate(10);
+        group.getChildren().addAll(one, two);
+        Scene scene = new Scene(group, 550, 500);
         stage.setScene(scene);
-        stage.setTitle("Evento click");
+        stage.setTitle("Ejemplo grupo de Layout");
         stage.show();
     }
 
