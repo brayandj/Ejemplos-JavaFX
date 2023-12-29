5548316511e196c5cfe0f6614b7c7e37abeccbdc
bryandj <bryandjw6@gmail.com>
2023-12-29 11:01:22 -0600

    Ejemplo layout con un TextArea

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -3,34 +3,33 @@ package com.jmc.readonlyprops;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        Group group = new Group();
-        Button one = new Button("one");
-        Button two = new Button("thow");
-        one.setLayoutX(100);
-        one.setLayoutY(10);
-        two.layoutXProperty().bind(one.layoutXProperty().add(one.widthProperty().add(10)));
-        two.layoutYProperty().bind(one.layoutYProperty());
-        group.setEffect(new DropShadow());
-        group.setRotate(10);
-        group.getChildren().addAll(one, two);
-        Scene scene = new Scene(group, 550, 500);
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setFillHeight(false);
+        Label label = new Label("Ey, prueba Layout");
+        TextArea textArea = new TextArea();
+        HBox.setHgrow(textArea, Priority.ALWAYS);
+        HBox.setMargin(textArea, new Insets(0, 10, 0, 10));
+        hBox.getChildren().addAll(label, textArea);
+        Scene scene = new Scene(hBox, 750, 500);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo grupo de Layout");
+        stage.setTitle("Ejemplo Layout HBOX");
         stage.show();
     }
 
