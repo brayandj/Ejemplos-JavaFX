b39b415c5f27eb08c73a939f7110f22f80d2a3ab
bryandj <bryandjw6@gmail.com>
2023-12-29 16:48:05 -0600

    Ejemplo layout con un FlowPane

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -3,6 +3,7 @@ package com.jmc.readonlyprops;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -18,16 +20,21 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
-        vBox.setAlignment(Pos.CENTER);
-        vBox.setFillWidth(true); //el VBox debe expandirse a todo lo ancho posible,  llenando el espacio horizontal de su nodo padre.
+        FlowPane flowPane = new FlowPane();
+        flowPane.setHgap(10);
+        flowPane.setVgap(10);
+        flowPane.setColumnHalignment(HPos.RIGHT);
+        Button button1 = new Button("Button 1");
+        Button button2 = new Button("Button 2");
+        Button button3 = new Button("Button 3");
         TextArea textArea = new TextArea();
-        VBox.setMargin(textArea, new Insets(0, 10, 0, 10));
-        vBox.getChildren().add(textArea);
-        Scene scene = new Scene(vBox, 500, 450);
+        Button button4 = new Button("Button 4");
+        Button button5 = new Button("Button 5");
+        Button button6 = new Button("Button 6");
+        flowPane.getChildren().addAll(button1, button2, button3,textArea, button4, button5, button6);
+        Scene scene = new Scene(flowPane, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo layout con un VBOX");
+        stage.setTitle("Ejemplo layout con un FlowPane");
         stage.show();
     }
 
