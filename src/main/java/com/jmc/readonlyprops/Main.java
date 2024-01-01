6e77114824c6d4318966307dbddf71b18c8c6f0f
bryandj <bryandjw6@gmail.com>
2023-12-31 23:16:45 -0600

    Ejemplo layout de un BorderPane

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -11,31 +11,54 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        FlowPane flowPane = new FlowPane();
-        flowPane.setHgap(10);
-        flowPane.setVgap(10);
-        flowPane.setColumnHalignment(HPos.RIGHT);
-        Button button1 = new Button("Button 1");
-        Button button2 = new Button("Button 2");
-        Button button3 = new Button("Button 3");
-        TextArea textArea = new TextArea();
-        Button button4 = new Button("Button 4");
-        Button button5 = new Button("Button 5");
-        Button button6 = new Button("Button 6");
-        flowPane.getChildren().addAll(button1, button2, button3,textArea, button4, button5, button6);
-        Scene scene = new Scene(flowPane, 500, 450);
+        BorderPane borderPane = new BorderPane();
+        ListView fruitList = new ListView<>();
+        ObservableList<String> items = FXCollections.observableArrayList();
+        items.add("Manzanas");
+        items.add("Naranjas");
+        items.add("Piñas");
+        fruitList.setItems(items);
+        //Top Nodos
+        Label topLabel = new Label("Applicación de frutas");
+        topLabel.setPadding(new Insets(20));
+        //Right Nodos
+        Label buttonLabel = new Label("Fruta añadida recientemente");
+        buttonLabel.setPadding(new Insets(10));
+        //Right Nodos
+        Label rightLabel = new Label("Número total de tipos de fruta: " + fruitList.getItems().size());
+        rightLabel.setRotate(90);
+        rightLabel.setPadding(new Insets(0, 0, 0, 60));
+        //Nodo Central
+        TextField field = new TextField();
+        Button button = new Button("Añadir");
+        HBox centerContainer = new HBox(10, field, button);
+        //Region ajustes BorderPane
+        borderPane.setTop(topLabel);
+        borderPane.setRight(rightLabel);
+        borderPane.setBottom(buttonLabel);
+        borderPane.setLeft(fruitList);
+        borderPane.setCenter(centerContainer);
+        //Registrar evento Handler
+        button.setOnMouseClicked(mouseEvent -> {
+            String item = field.getText();
+            items.add(item);
+            buttonLabel.setText("Fruta añadida recientemente: " + item);
+            rightLabel.setText("Total de fruta");
+            rightLabel.setText("Número total de tipos de fruta: " + fruitList.getItems().size());
+            field.setText("");
+            field.requestFocus();
+        });
+        Scene scene = new Scene(borderPane, 700, 650);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo layout con un FlowPane");
+        stage.setTitle("Ejemplo layout de un BorderPane");
         stage.show();
+
     }
 
     public static void main(String[] args) {
