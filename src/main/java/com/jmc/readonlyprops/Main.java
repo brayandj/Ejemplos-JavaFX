1c47ec7c2d81ee229d950a70f459b869a42ecd9a
bryandj <bryandjw6@gmail.com>
2024-01-03 13:57:54 -0600

    Ejemplo de control MenuButton

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,40 +1,28 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        BorderPane root = new BorderPane();
-        HBox linksLaout = new HBox();
-        linksLaout.setAlignment(Pos.CENTER);
-        Hyperlink linkOne = new Hyperlink("GitHub");
-        Hyperlink linkTwo = new Hyperlink("Udemy");
-        WebView webView = new WebView();
-
-        linkOne.setOnAction(actionEvent -> {
-            webView.getEngine().load("https://github.com/");
-        });
-        linkTwo.setOnAction(actionEvent -> {
-            webView.getEngine().load("https://www.udemy.com");
-        });
-        linksLaout.getChildren().addAll(linkOne, linkTwo);
-        root.setTop(linksLaout);
-        root.setCenter(webView);
-        Scene scene = new Scene(root,500, 450);
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        MenuButton sites = new MenuButton("Sitios");
+        MenuItem google = new MenuItem("Google");
+        MenuItem youtube = new MenuItem("Youtube");
+        google.setOnAction(actionEvent -> System.out.println("Google.com"));
+        youtube.setOnAction(actionEvent -> System.out.println("Youtube.com"));
+        sites.getItems().addAll(google, youtube);
+        root.getChildren().addAll(sites);
+        Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de control Hyperlink");
+        stage.setTitle("Ejemplo de control MenuButton");
         stage.show();
     }
 
