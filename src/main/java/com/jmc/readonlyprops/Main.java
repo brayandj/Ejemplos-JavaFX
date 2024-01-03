cfc0b185a50163180dfd6c44b0135e2ae67d9afb
bryandj <bryandjw6@gmail.com>
2024-01-03 11:35:32 -0600

    Ejemplo de control Label

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,44 +1,32 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        TextFlow root = new TextFlow();
-        Text heading = new Text("Lorem ipsum");
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
 
-        heading.setFill(Color.BLUE);
-        heading.setFont(Font.font("Arial", FontWeight.BOLD, 18));
-        Text paramgraph = new Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ea, esse eius quis quas quisquam dignissimos " +
-                "dolorem vitae unde nihil sunt totam error pariatur quos obcaecati est consequuntur. Voluptatibus, itaque? " +
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam ea, esse eius quis quas quisquam dignissimos \" +\n" +
-                "                \"dolorem vitae unde nihil sunt totam error pariatur quos obcaecati est consequuntur. Voluptatibus, itaque"
-                );
-
-        root.setLineSpacing(5);
-        root.setTextAlignment(TextAlignment.CENTER);
-        root.setPrefWidth(300);
-        root.getChildren().addAll(heading, paramgraph);
-        Scene scene = new Scene(root);
+        Label label = new Label("Texto de prueba");
+        Rectangle rectangle = new Rectangle(20, 20, Color.BLUE);
+        label.setGraphic(rectangle);
+        label.setUnderline(true);
+        label.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        label.setTextFill(Color.GREEN);
+        root.getChildren().addAll(label);
+        Scene scene = new Scene(root,500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo layout TextFlow");
+        stage.setTitle("Ejemplo de control Label");
         stage.show();
     }
 
