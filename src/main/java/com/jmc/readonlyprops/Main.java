03243dc794f6b6b0d0e7d3ecd1b9738b3e3d55f8
bryandj <bryandjw6@gmail.com>
2024-01-04 09:17:18 -0600

    Ejemplo de control RadioButton

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,6 +1,8 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -8,30 +10,38 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    Label userSelection;
+    @Override
+    public void init() throws Exception {
+        userSelection = new Label("Nada seleccionado aún");
+    }
+
     @Override
     public void start(Stage stage) {
-        VBox root = new VBox();
+        VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
-        Label label = new Label("Seccion de las estaciónes: ");
-        ToggleButton spring = new ToggleButton("Primavera");
-        ToggleButton summer = new ToggleButton("Verano");
-        ToggleButton fall = new ToggleButton("Otoño");
-        ToggleButton winter = new ToggleButton("Invierno");
+        RadioButton spring = new RadioButton("Primavera");
+        RadioButton summer = new RadioButton("Verano");
+        RadioButton fall = new RadioButton("Otoño");
+        RadioButton winter = new RadioButton("Invierno");
 
-        ToggleGroup seasons = new ToggleGroup();
-        seasons.getToggles().addAll(spring, summer, fall, winter);
-        HBox seasonsLayout = new HBox();
-        seasonsLayout.setAlignment(Pos.CENTER);
-        seasonsLayout.setSpacing(5);
-        seasonsLayout.getChildren().addAll(spring, summer, fall, winter);
-        root.getChildren().addAll(label, seasonsLayout);
-        seasons.selectedToggleProperty().addListener((observableValue, toggle, t1) -> {
-        label.setText("La selección de las cuatro estaciones es: " + ((Labeled)t1).getText());
-        });
+        ToggleGroup group = new ToggleGroup();
+        group.getToggles().addAll(spring, summer, fall, winter);
+        group.selectedToggleProperty().addListener(this::changed);
+        root.getChildren().addAll(userSelection, spring, summer, fall, winter);
+        
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de control ToggleButton");
+        stage.setTitle("Ejemplo de control RadioButton");
         stage.show();
+    }
+
+    private void changed(ObservableValue<? extends Toggle> observable, Toggle oldBtn, Toggle newBtn) {
+        String selectedLabel = "None";
+        if (newBtn != null) {
+            selectedLabel = ((Labeled)newBtn).getText();
+        }
+        userSelection.setText("Tu selección: " + selectedLabel);
     }
 
     public static void main(String[] args) {
