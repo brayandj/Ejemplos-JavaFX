eef6dc18144b57c42a86b11c4ee4fbec01a2308b
Brayan Diaz <bryandjw6@gmail.com>
2024-01-06 10:16:02 -0600

    Ejemplo de un control ChoiceBox

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -3,46 +3,36 @@ package com.jmc.readonlyprops;
 import javafx.application.Application;
 import javafx.beans.Observable;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    Label answer;
-    CheckBox checkBox;
-    @Override
-    public void init() throws Exception {
-        answer = new Label("Tu respuesta: None");
-        checkBox = new CheckBox("Selecciónes");
-    }
+import java.util.Collection;
 
+public class Main extends Application {
     @Override
     public void start(Stage stage) {
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10);
-        Label question = new Label("Estás a favor?");
-        checkBox.setAllowIndeterminate(true);
-        checkBox.selectedProperty().addListener(this::changed);
-        checkBox.indeterminateProperty().addListener(this::changed);
+        Label label = new Label("Tu selección");
 
-        root.getChildren().addAll(answer, question, checkBox);
+        ChoiceBox<String> directions = new ChoiceBox<>();
+        ObservableList<String> diriectionList = FXCollections.observableArrayList("Norte", "Sur", "Este", "Oeste");
+        directions.getItems().addAll(diriectionList);
+        directions.getSelectionModel().selectNext();
+        directions.getSelectionModel().selectedIndexProperty().addListener(observable -> label.setText("Tu rirección: " +
+                directions.getSelectionModel().getSelectedItem()));
+
+        root.getChildren().addAll(label, directions);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de control CheckBox");
+        stage.setTitle("Ejemplo de control ChoiceBox");
         stage.show();
-    }
-
-    private void changed(ObservableValue<? extends Boolean> observableValue, Boolean olVal, Boolean newVal) {
-        String choice;
-        if (checkBox.isIndeterminate()) {
-            choice = "Neutral";
-        } else {
-            choice = "En desacuerdo";
-        }
-        answer.setText("Tu respuesta: " + choice);
     }
 
     public static void main(String[] args) {
