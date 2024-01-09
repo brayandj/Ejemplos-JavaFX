1f3fd052e499a311fd74d09b2f63c0909266c694
Brayan Diaz <bryandjw6@gmail.com>
2024-01-08 20:04:20 -0600

    Ejemplo de un control ColorPicker

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,8 +1,10 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -12,14 +14,12 @@ public class Main extends Application {
     public void start(Stage stage) {
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
-        ComboBox<String> names = new ComboBox<>();
-        names.getItems().addAll("César", "Fernando", "Benjamín");
-        names.setCellFactory(listView -> new ShapeNameCell());
-        names.setButtonCell(new ShapeNameCell());
-        root.getChildren().addAll(names);
+        ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setOnAction(actionEvent -> root.setBackground(new Background(new BackgroundFill(colorPicker.getValue(), CornerRadii.EMPTY, Insets.EMPTY))));
+        root.getChildren().addAll(colorPicker);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de un control ComboBox");
+        stage.setTitle("Ejemplo de un control ColorPicker");
         stage.show();
     }
 
