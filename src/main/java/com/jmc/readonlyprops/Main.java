f22159d2af2fbdbdd58bd71d1241123506dc5fb0
Brayan Diaz <bryandjw6@gmail.com>
2024-01-13 09:14:29 -0600

    Ejemplo de un control Slider

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,29 +1,36 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
-        Label date = new Label();
-        DatePicker datePicker = new DatePicker();
-        datePicker.setEditable(true);
-        datePicker.setOnAction(actionEvent -> date.setText(datePicker.getValue().toString()));
-        root.getChildren().addAll(date, datePicker);
+        Slider red = new Slider(0, 255, 0);
+        Slider green = new Slider(0, 255, 0);
+        Slider blue = new Slider(0, 255, 0);
+        Slider[] sliders = {red, green, blue};
+        for (Slider slider: sliders) {
+            slider.setShowTickMarks(true);
+            slider.setBlockIncrement(5);
+            slider.valueProperty().addListener(o -> {
+                int redColor = (int)red.getValue();
+                int greenColor = (int)green.getValue();
+                int blueColor = (int)blue.getValue();
+                root.setStyle(String.format("-fx-background-color: rgb(%d, %d, %d);", redColor, greenColor, blueColor));
+            });
+        }
+        root.getChildren().addAll(red, green, blue);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de un control DatePicker");
+        stage.setTitle("Ejemplo de un control Slider");
         stage.show();
     }
 
