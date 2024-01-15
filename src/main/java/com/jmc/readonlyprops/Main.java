b0597ce7fc548fa720b1b20204855e5ffa92e890
bryandj <bryandjw6@gmail.com>
2024-01-15 13:00:12 -0600

    Ejemplo de un control ListView

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -2,6 +2,9 @@ package com.jmc.readonlyprops;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -13,23 +16,19 @@ import javafx.scene.layout.VBox;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        BorderPane root = new BorderPane();
-        TabPane pane = new TabPane();
-        Tab one = new Tab("Uno");
-        Tab two = new Tab("Dos");
-        VBox contentOne = new VBox();
-        contentOne.setAlignment(Pos.CENTER);;
-        contentOne.getChildren().addAll(new Label("Contenido para un Tab"));
-        VBox contentTwo = new VBox();
-        contentTwo.setAlignment(Pos.CENTER);;
-        contentTwo.getChildren().addAll(new Button("Contenido Tab"));
-        one.setContent(contentOne);
-        two.setContent(contentTwo);
-        pane.getTabs().addAll(one, two);
-        root.setCenter(pane);
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        Label selectedItemList = new Label("Primer selección en la lista");
+        Button btnSelectedFirst = new Button("Primer item");
+        ListView<String> list = new ListView<>();
+        ObservableList<String> names = FXCollections.observableArrayList("Fernándo", "César", "Benjamín", "Gerardo", "Brayan", "Roxana");
+        list.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> selectedItemList.setText("" + newValue));
+        list.getItems().addAll(names);
+        btnSelectedFirst.setOnAction(actionEvent -> list.getSelectionModel().selectFirst());
+        root.getChildren().addAll(selectedItemList, list, btnSelectedFirst);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de un control TabPane");
+        stage.setTitle("Ejemplo de un control ListView");
         stage.show();
     }
 
