82679283ceedeff951dbd1c115139c23f263713d
bryandj <bryandjw6@gmail.com>
2024-01-15 10:34:51 -0600

    Ejemplo de un control MenuBar

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,9 +1,11 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 
@@ -11,26 +13,27 @@ import javafx.scene.layout.VBox;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        VBox root = new VBox(10);
-        root.setAlignment(Pos.CENTER);
-        Slider red = new Slider(0, 255, 0);
-        Slider green = new Slider(0, 255, 0);
-        Slider blue = new Slider(0, 255, 0);
-        Slider[] sliders = {red, green, blue};
-        for (Slider slider: sliders) {
-            slider.setShowTickMarks(true);
-            slider.setBlockIncrement(5);
-            slider.valueProperty().addListener(o -> {
-                int redColor = (int)red.getValue();
-                int greenColor = (int)green.getValue();
-                int blueColor = (int)blue.getValue();
-                root.setStyle(String.format("-fx-background-color: rgb(%d, %d, %d);", redColor, greenColor, blueColor));
-            });
-        }
-        root.getChildren().addAll(red, green, blue);
+        BorderPane root = new BorderPane();
+        MenuBar menu = new MenuBar();
+        Menu file = new Menu("Archivo");
+        MenuItem save = new MenuItem("Guardar");
+        MenuItem exit = new MenuItem("Salir");
+        exit.setOnAction(actionEvent -> Platform.exit());
+        file.getItems().addAll(save, exit);
+
+        Menu edit = new Menu("Edit");
+        RadioMenuItem option = new RadioMenuItem("Opciones");
+        RadioMenuItem settings = new RadioMenuItem("Configuración");
+        MenuItem prefOne = new MenuItem("Pref: Uno");
+        MenuItem prefTwo = new MenuItem("Pref: Dos");
+        Menu prefs = new Menu("Preferencias");
+        prefs.getItems().addAll(prefOne, prefTwo);
+        edit.getItems().addAll(option, settings, new SeparatorMenuItem(), prefs);
+        menu.getMenus().addAll(file, edit);
+        root.setTop(menu);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de un control Slider");
+        stage.setTitle("Ejemplo de un control MenuBar");
         stage.show();
     }
 
