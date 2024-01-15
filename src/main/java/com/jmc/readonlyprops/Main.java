f5d5db0d72f751879239c6b4f59b1dc8916353db
bryandj <bryandjw6@gmail.com>
2024-01-15 11:23:50 -0600

    Ejemplo de un control TabPane

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -14,26 +14,22 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
-        MenuBar menu = new MenuBar();
-        Menu file = new Menu("Archivo");
-        MenuItem save = new MenuItem("Guardar");
-        MenuItem exit = new MenuItem("Salir");
-        exit.setOnAction(actionEvent -> Platform.exit());
-        file.getItems().addAll(save, exit);
-
-        Menu edit = new Menu("Edit");
-        RadioMenuItem option = new RadioMenuItem("Opciones");
-        RadioMenuItem settings = new RadioMenuItem("Configuración");
-        MenuItem prefOne = new MenuItem("Pref: Uno");
-        MenuItem prefTwo = new MenuItem("Pref: Dos");
-        Menu prefs = new Menu("Preferencias");
-        prefs.getItems().addAll(prefOne, prefTwo);
-        edit.getItems().addAll(option, settings, new SeparatorMenuItem(), prefs);
-        menu.getMenus().addAll(file, edit);
-        root.setTop(menu);
+        TabPane pane = new TabPane();
+        Tab one = new Tab("Uno");
+        Tab two = new Tab("Dos");
+        VBox contentOne = new VBox();
+        contentOne.setAlignment(Pos.CENTER);;
+        contentOne.getChildren().addAll(new Label("Contenido para un Tab"));
+        VBox contentTwo = new VBox();
+        contentTwo.setAlignment(Pos.CENTER);;
+        contentTwo.getChildren().addAll(new Button("Contenido Tab"));
+        one.setContent(contentOne);
+        two.setContent(contentTwo);
+        pane.getTabs().addAll(one, two);
+        root.setCenter(pane);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de un control MenuBar");
+        stage.setTitle("Ejemplo de un control TabPane");
         stage.show();
     }
 
