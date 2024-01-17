3f7d2accdd675875beae65897b51318bcf116eec
bryandj <bryandjw6@gmail.com>
2024-01-17 11:28:58 -0600

    Ejemplo de un control TreeView parte 1

--- src/main/java/com/jmc/readonlyprops/Main.java
+++ src/main/java/com/jmc/readonlyprops/Main.java
@@ -1,6 +1,7 @@
 package com.jmc.readonlyprops;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -10,20 +11,34 @@ import javafx.scene.layout.VBox;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        VBox root = new VBox();
-        TableView<User> userTable = new TableView<>(UserUtility.getUserList());
-        userTable.setEditable(true);
-        userTable.getColumns().addAll(
-                UserUtility.getFirstNameColumn(),
-                UserUtility.getLastNameColumn(),
-                UserUtility.getNatDateColumn(),
-                UserUtility.getDeleteUserColumn()
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        TreeView<String> treeView = new TreeView<>();
+        TreeItem<String> classONe = new TreeItem<>("Class One");
+        classONe.setExpanded(true);
+        classONe.getChildren().addAll(
+                new TreeItem<>("Inóngo"),
+                new TreeItem<>("Negro"),
+                new TreeItem<>("Firulais"),
+                new TreeItem<>("Caracol")
         );
-        userTable.getSelectionModel().selectFirst();
-        root.getChildren().addAll(userTable);
+        TreeItem<String> classTwo = new TreeItem<>("Class Two");
+        classTwo.setExpanded(true);
+        classTwo.getChildren().addAll(
+                new TreeItem<>("César"),
+                new TreeItem<>("Gerarado"),
+                new TreeItem<>("Fernándo"),
+                new TreeItem<>("César")
+        );
+        classTwo.addEventHandler(TreeItem.branchExpandedEvent(),e -> System.out.println(e.getTreeItem().getValue()));
+        TreeItem<String> classes = new TreeItem<>("Clasess");
+        classes.getChildren().addAll(classONe, classTwo);
+        treeView.setRoot(classes);
+        treeView.setShowRoot(false);
+        root.getChildren().addAll(treeView);
         Scene scene = new Scene(root, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("Ejemplo de un control ListView");
+        stage.setTitle("Ejemplo de un control TreeView parte 1");
         stage.show();
     }
 
